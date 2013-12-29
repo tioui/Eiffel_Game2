0d6c7564d0224733207bb8166cd19b5e8208f773
Louis M <prog@tioui.com>
2013-12-29 13:38:03 -0500

    Removing old C files.

--- audio_video_lib/implementation/Clib_old/include/av_addition.h
+++
@@ -1,8 +0,0 @@
-#ifndef AV_MORE_H
-#define AV_MORE_H
-#include "cpf_more.h"
-
-int av_more_read_packet(void *opaque, uint8_t *buf, int buf_size);
-int64_t av_more_seek(void *opaque, int64_t offset, int whence);
-
-#endif
